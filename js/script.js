e50cc88441763d365935d303b8972ed2ca478d1d
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-01-29 18:11:11 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,33 +1,61 @@
-"use strict";
-class slideStories {
+class SlideStories {
   constructor(id) {
     this.slide = document.querySelector(`[data-slide="${id}"]`);
     this.active = 0;
-    this.activeSlide(0);
     this.init();
   }
+
   activeSlide(index) {
     this.active = index;
-    this.items = this.slide.querySelectorAll(".slide-items  > *");
     this.items.forEach((item) => item.classList.remove("active"));
     this.items[index].classList.add("active");
+    this.thumbItems.forEach((item) => item.classList.remove("active"));
+    this.thumbItems[index].classList.add("active");
+    this.autoSlide();
   }
 
-  prev() {}
+  prev() {
+    if (this.active > 0) {
+      this.activeSlide(this.active - 1);
+    } else {
+      this.activeSlide(this.items.length - 1);
+    }
+  }
 
   next() {
-    this.activeSlide(this.active + 1);
+    if (this.active < this.items.length - 1) {
+      this.activeSlide(this.active + 1);
+    } else {
+      this.activeSlide(0);
+    }
   }
 
-  addNavegation() {
+  addNavigation() {
     const nextBtn = this.slide.querySelector(".slide-next");
+    const prevBtn = this.slide.querySelector(".slide-prev");
     nextBtn.addEventListener("click", this.next);
+    prevBtn.addEventListener("click", this.prev);
+  }
+
+  addThumbItems() {
+    this.items.forEach(() => (this.thumb.innerHTML += `<span></span>`));
+    this.thumbItems = Array.from(this.thumb.children);
+  }
+
+  autoSlide() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(this.next, 5000);
   }
 
   init() {
     this.next = this.next.bind(this);
-    this.addNavegation();
+    this.prev = this.prev.bind(this);
+    this.items = this.slide.querySelectorAll(".slide-items > *");
+    this.thumb = this.slide.querySelector(".slide-thumb");
+    this.addThumbItems();
+    this.activeSlide(0);
+    this.addNavigation();
   }
 }
 
-new slideStories("slide");
+new SlideStories("slide");
