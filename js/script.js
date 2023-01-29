d8f0d8210b6cb2468de55a0d1c5a53cb2962df6a
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-01-29 17:08:05 +0000

    update

--- js/script.js
+++ js/script.js
@@ -3,12 +3,30 @@ class slideStories {
   constructor(id) {
     this.slide = document.querySelector(`[data-slide="${id}"]`);
     this.active = 0;
-    this.activeSlide(2);
+    this.activeSlide(0);
+    this.init();
   }
   activeSlide(index) {
     this.active = index;
     this.items = this.slide.querySelectorAll(".slide-items  > *");
+    this.items.forEach((item) => item.classList.remove("active"));
     this.items[index].classList.add("active");
+  }
+
+  prev() {}
+
+  next() {
+    this.activeSlide(this.active + 1);
+  }
+
+  addNavegation() {
+    const nextBtn = this.slide.querySelector(".slide-next");
+    nextBtn.addEventListener("click", this.next);
+  }
+
+  init() {
+    this.next = this.next.bind(this);
+    this.addNavegation();
   }
 }
 
